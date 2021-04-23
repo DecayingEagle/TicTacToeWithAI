a4f9eeeda741bec9936c13019b3bdd1f3ec957f0
DecayingEAgle <therealeagle.yt@gmail.com>
2021-04-23 15:54:47 +0200

    First working shit 0.0.1v

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
+char tile[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+
 int main()
 {
-    cout << "Hello World" << endl;
+    cout << " " << tile[0] << " | " << tile[1] << " | " << tile[2] << " \n";
+    cout << "-----------\n";
+    cout << " " << tile[3] << " | " << tile[4] << " | " << tile[5] << " \n";
+    cout << "-----------\n";
+    cout << " " << tile[6] << " | " << tile[7] << " | " << tile[8] << " \n";
+    return 0;
 }
